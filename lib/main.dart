69415628e195d247a51cb6a4ad2334514bd2d17f
aktasilyas <aktasilyas875@gmail.com>
2024-02-22 00:17:02 +0300

    rastgele zar atma işlemi yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(const Color.fromARGB(255, 9, 60, 200), const Color.fromARGB(255, 182, 200, 19)),
+      body: GradientContainer( Color.fromARGB(255, 9, 60, 200),  Color.fromARGB(255, 182, 200, 19)),
     ),
   ));
 }
